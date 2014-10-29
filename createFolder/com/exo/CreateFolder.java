508e497959c26a1dc41bf7596080a6f50f2712e0
dandy-okami <vysconti@gmail.com>
2014-10-29 19:53:15 +0100

    version 0.2

---
+++ createFolder/com/exo/CreateFolder.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.exo;
+
+/**
+ * @author dandy-kun
+ *
+ */
+public class CreateFolder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
